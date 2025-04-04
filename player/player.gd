08a990fcd94cf8c3f40c565ee59d93ff27e144b4
ben <bwall4@gmu.edu>
2025-04-04 19:43:57 -0400

    bare minimum movement

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 class_name Player
 
-const H_VEL = 500.0
-const H_ACCEL = 1500.0
+const H_VEL = 1024.0
+const H_ACCEL = 4096.0
 
 func h_input() -> float:
 	return Input.get_axis("left", "right")
@@ -11,13 +11,14 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	
 	var target_x_vel := h_input() * H_VEL
-	var x_accel: float = sign(target_x_vel - velocity.x)
+	print(target_x_vel)
+	var x_accel: float = sign(target_x_vel - velocity.x) * H_ACCEL
 	var x_accel_integrated = x_accel * delta
 	
 	if abs(x_accel_integrated) > abs(target_x_vel - velocity.x):
 		x_accel_integrated = target_x_vel - velocity.x
+	print(x_accel_integrated)
 	
 	velocity.x += x_accel_integrated
-	
 	
 	move_and_slide()
