ea2768bab687c4192551ecd36a0770a3cf4f7902
ben <bwall4@gmu.edu>
2025-04-05 14:49:26 -0400

    spring physics?

--- player/tail.gd
+++ player/tail.gd
@@ -12,16 +12,38 @@ var current_offset: Vector2 = Vector2.ZERO
 
 var _last_position = global_position
 
+var spring_x: Vector2 = Vector2.ZERO
+var spring_v: Vector2 = Vector2.ZERO
+
+const K = 800
+const SEP = 0
+const B = 30
+
+func spring_physics(delta: float, parent_x: Vector2) -> void:
+	var dir := spring_x - parent_x
+	var dist := dir.length()
+	var force := Vector2.ZERO
+	if dist > 0:
+		dir /= dist
+		force += -K * (dist - SEP) * dir - B * spring_v
+		
+	spring_v += force * delta
+	spring_x += spring_v * delta
+	# print(force)
+
 func _physics_process(delta: float) -> void:
 	var p = get_parent()
 	if p is TailPhysics:
-		current_offset += (p.current_offset - current_offset) * Global.get_lerp(0.3, delta)
+		spring_physics(delta, p.spring_x)
+		current_offset = spring_x
+		#current_offset += (p.current_offset - current_offset) * Global.get_lerp(0.3, delta)
 		position = original_position + current_offset
 	else:
 		current_offset = _last_position - global_position
 		#var noise_amount = clamp(current_offset.length() / 16.0, 0.0, 1.0)
 		#current_offset += current_offset.orthogonal() * noise_amount
-		current_offset = current_offset.limit_length(16)
+		#current_offset = current_offset.limit_length(16)
+		spring_x = current_offset
 		#print(current_offset)
 		_last_position = global_position
 		
